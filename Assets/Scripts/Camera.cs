debaae6c3ed8301645eefd9afb63b87cc0a50794
Ambrose McJunkin <Jerorfigan@gmail.com>
2014-01-25 19:23:41 -0700

    Added new prototype level (level 2). Added camera control script to camera object that makes camera track player object. Added curved wall prefab.

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public Vector3 myPos = new Vector3(0,0,-3);
+	
+	public Transform myPlay;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = myPlay.position + myPos;
+	}
+}
