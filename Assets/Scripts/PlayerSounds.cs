7614934c1367076b84198bb66f02f3789b744353
Tyson Joehler <tjoehler@gmail.com>
2014-01-26 12:08:26 -0700

    Play squeak sounds when the echo pulse is triggered.

--- Assets/Scripts/PlayerSounds.cs
+++ Assets/Scripts/PlayerSounds.cs
@@ -6,9 +6,11 @@ using System.Collections;
 public class PlayerSounds : MonoBehaviour {
 
 	public PlayerController player;
+	public EchoSphere echos;
 
 	public AudioClip[] flapUpSounds;
 	public AudioClip[] flapDownSounds;
+	public AudioClip[] squeakSounds;
 	
 	// Use this for initialization
 	void Start () {
@@ -17,12 +19,30 @@ public class PlayerSounds : MonoBehaviour {
 		}
 		player.OnWingsFlapStarted += HandleOnWingsFlapStarted;
 		player.OnWingsFlapEnded += HandleOnWingsFlapEnded;
+
+		if (echos != null) {
+			echos.OnPulseStarted += HandleOnPulseStarted;
+		} else {
+			Debug.LogWarning ("Must assign the echo script to player sounds");
+		}
+	}
+
+	void HandleOnPulseStarted ()
+	{
+		// play a squeak sound.
+		if (squeakSounds.Length > 0) {
+			audio.PlayOneShot (squeakSounds[ Random.Range(0, squeakSounds.Length) ]);
+		} else {
+			Debug.LogWarning ("No squeak sounds configured");
+		}
 	}
 
 	void HandleOnWingsFlapEnded ()
 	{
 		if (flapUpSounds.Length > 0) {
 			audio.PlayOneShot (flapUpSounds[ Random.Range(0, flapUpSounds.Length) ]);
+		} else {
+			Debug.LogWarning ("No flap up sounds configured");
 		}
 	}
 
@@ -30,6 +50,8 @@ public class PlayerSounds : MonoBehaviour {
 	{
 		if (flapDownSounds.Length > 0) {
 			audio.PlayOneShot (flapDownSounds[ Random.Range(0, flapDownSounds.Length) ]);
+		} else {
+			Debug.LogWarning ("No flap down sounds configured");
 		}
 	}
 }
