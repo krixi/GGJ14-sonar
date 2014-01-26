43ac5761d854df6defae1986d0d920c7aa9b4027
Tyson Joehler <tjoehler@gmail.com>
2014-01-26 12:44:27 -0700

    Added game intro and finished scenes. Added text display of the current cave number.

---
+++ Assets/Scripts/LoadLevelOnStart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelOnStart : MonoBehaviour {
+
+	public string levelToLoad = "Level1";
+
+	// Use this for initialization
+	void Start () {
+		Application.LoadLevel (levelToLoad);
+	}
+}
