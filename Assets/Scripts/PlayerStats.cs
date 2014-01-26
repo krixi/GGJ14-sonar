d11bb3e8dc637a82c8f6b25a2fd371932b6e9afb
Tyson Joehler <tjoehler@gmail.com>
2014-01-26 11:32:50 -0700

    Added energy and health bars.

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -9,6 +9,16 @@ public class PlayerStats : MonoBehaviour {
 	public float health = 100f;
 
 	/// <summary>
+	/// The max health.
+	/// </summary>
+	public float maxHealth = 100f;
+
+	/// <summary>
+	/// The health bar.
+	/// </summary>
+	public GUIProgressBar healthBar;
+
+	/// <summary>
 	/// The energy.
 	/// </summary>
 	public float energy = 100f;
@@ -23,12 +33,20 @@ public class PlayerStats : MonoBehaviour {
 	/// </summary>
 	public float energyRestoreRate = 5f;
 
+	/// <summary>
+	/// The energy bar.
+	/// </summary>
+	public GUIProgressBar energyBar;
+
 	// Update is called once per frame
 	void Update () {
 		//energy = Mathf.Min (maxEnergy, Mathf.Lerp (energy, maxEnergy, energyRestoreRate * Time.deltaTime));
 		energy += energyRestoreRate * Time.deltaTime;
 		if (energy > maxEnergy ) {
 			energy = maxEnergy;
+		}
+		if (energyBar != null) {
+			energyBar.barDisplay = (energy / maxEnergy);
 		}
 	}
 
@@ -38,5 +56,8 @@ public class PlayerStats : MonoBehaviour {
 	/// <param name="_h">_h.</param>
 	public void SetHealth (float _h) {
 		health = _h;
+		if (healthBar != null) {
+			healthBar.barDisplay = (health / maxHealth);
+		}
 	}
 }
